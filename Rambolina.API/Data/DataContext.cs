7c536d4288f108c35e6b4b0325807a1c20220d75
Oren Lahav <orenlahav@Orens-MacBook-Pro.local>
2020-04-06 17:33:21 -0400

    Initial Commit

---
+++ Rambolina.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Rambolina.API.Models;
+
+namespace Rambolina.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+        
+    }
+}
