3f51ccd9d799398585a108159f2f169900346af3
Oren Lahav <orenlahav@Orens-MacBook-Pro.local>
2020-04-24 15:03:24 -0400

    Client side login

--- Rambolina.API/Controllers/ValuesController.cs
+++ Rambolina.API/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace Rambolina.API.Controllers
             _context = context;
 
         }
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
